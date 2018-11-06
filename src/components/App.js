aa79443c2d61b4c9006530cc7d016819a87f5886
Nick Sladden <nickoliasxii@gmail.com>
2018-11-06 08:39:24 -0600

    getting typeerror from DATA file, but have the correct code so whatever

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,23 @@ import { connect } from 'react-redux'
 import { handleInitialData } from '../store/actions/shared'
 
 import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
+import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData())
   }
   render() {
-    return <div>{this.props.loading === true ? null : <Dashboard />}</div>
+    return (
+      <div>
+        <LoadingBar />
+        {this.props.loading === true ? null : (
+          <TweetPage match={{ params: { id: '8xf0y6ziyjabvozdd253nd' } }} />
+        )}
+      </div>
+    )
   }
 }
 
--- src/store/reducers/tweets.js
+++ src/store/reducers/tweets.js
@@ -1,4 +1,4 @@
-import { RECEIVE_TWEETS } from '../actions/tweets'
+import { RECEIVE_TWEETS, TOGGLE_TWEET, ADD_TWEET } from '../actions/tweets'
 
 export default function tweets(state = {}, action) {
   switch (action.type) {
@@ -6,6 +6,38 @@ export default function tweets(state = {}, action) {
       return {
         ...state,
         ...action.tweets
+      }
+    case TOGGLE_TWEET:
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          likes:
+            action.hasLiked === true
+              ? //removes username if they've already liked the tweet
+                state[action.id].likes.filter(uid => uid !== action.authedUser)
+              : //adds the username if they have not already liked the tweet
+                state[action.id].likes.concat([action.authedUser])
+        }
+      }
+    case ADD_TWEET:
+      const { tweet } = action
+      let replyingTo = {}
+
+      if (tweet.replyingTo !== null) {
+        replyingTo = {
+          [tweet.replyingTo]: {
+            ...state[tweet.replyingTo],
+            replies: state[tweet.replyingTo].replies.concat([tweet.id])
+          }
+        }
+      }
+      return {
+        ...state,
+        //adds new tweet to tweets array
+        [action.tweet.id]: action.tweet,
+        //if new tweet is a reply to a previous tweet, spreads previous replies and adds new tweet to those replies
+        ...replyingTo
       }
     default:
       return state
